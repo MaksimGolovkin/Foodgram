0deaa641bb826a7e594471023492bf9a140225f2
Maksim Golovkin <maxmaxironfist@gmail.com>
2024-12-27 01:17:08 +0300

    Fourth

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -4,6 +4,6 @@ from api.constant import MAX_PAGE_SIZE, PAGINATOR_PAGE_SIZE
 
 
 class FoodgramPagination(PageNumberPagination):
-    page_size = PAGINATOR_PAGE_SIZE  # 6
     page_size_query_param = 'limit'
-    max_page_size = MAX_PAGE_SIZE  # 10
+    page_size = PAGINATOR_PAGE_SIZE  # 6
+    # max_page_size = MAX_PAGE_SIZE  # 10
